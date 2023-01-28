5b617b299422d7bfcdb99d17d5315869cc233751
supersmaskens <116157407+supersmaskens@users.noreply.github.com>
2023-01-28 13:05:42 +0100

    Delete laboration-1-master directory

--- laboration-1-master/Movable.java
+++
@@ -1,9 +0,0 @@
-/**
- * Interface for movable objects.
- * NOTE: Does not incluude any required attributes (X position, direction, etc).
- */
-public interface Movable {
-    void move();
-    void turnLeft();
-    void turnRight();
-}
